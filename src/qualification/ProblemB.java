683c9b51a0426a8440b02fadf6744bda041601dc
rodders1991 <georgeormrod@gmail.com>
2016-01-05 20:33:20 +0000

    Added Qualification round files
    Added the skeleton files for the qualification round

---
+++ src/qualification/ProblemB.java
@@ -0,0 +1,10 @@
+package qualification;
+
+public class ProblemB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/qualification/ProblemC.java
@@ -0,0 +1,10 @@
+package qualification;
+
+public class ProblemC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
